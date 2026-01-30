a364e9a92edc443ddc00585f3ae64e526d0480c0
jzajas <jzajas1@gmail.com>
2026-01-30 12:24:32 +0100

    initial setup

---
+++ backend/src/main/java/com/jzajas/network_management/NetworkManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jzajas.network_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkManagementApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jzajas/network_management/NetworkManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzajas.network_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
